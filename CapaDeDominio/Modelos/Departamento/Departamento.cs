2026a93dccbb3f002a1c48b3ef6634f430e83c0e
wuesis <wuesis983@gmail.com>
2022-03-31 00:20:16 -0600

    Proyecto creado y modelo de departamento listo con data anottations

---
+++ CapaDeDominio/Modelos/Departamento/Departamento.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDeDominio
+{
+    public class Departamento : IDepartamento
+    {
+        public int IdDepartamento { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Se necesita un nombre para el departemento.")]
+        [StringLength(20, ErrorMessage = "El departamento debe tener un nombre mas grande.", MinimumLength = 2)]
+        public string NombreDepartamento { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Se necesita el telefono del departemento.")]
+        [StringLength(10, ErrorMessage = "El numero debe ser a 10 digitos.", MinimumLength = 10)]
+        [RegularExpression(@"^([\+\d{1,2}\s]+\d{10})$")]
+        public string NumeroTelefono { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Se necesita el telefono del departemento.")]
+        [RegularExpression(@"^[0-9a-zA-Z_\-\.]+)@([0-9a-zA-Z_\-\.])\.([a-zA-Z]{2,5})$")]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Se necesita el telefono del departemento.")]
+        public string Ciudad { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Se necesita saber el estado donde se ubica el departemento.")]
+        public string Estado { get; set; }
+
+    }
+}
---
+++ CapaDeDominio/Modelos/Departamento/IDepartamento.cs
@@ -0,0 +1,12 @@
+﻿namespace CapaDeDominio
+{
+    public interface IDepartamento
+    {
+        string Ciudad { get; set; }
+        string Email { get; set; }
+        string Estado { get; set; }
+        int IdDepartamento { get; set; }
+        string NombreDepartamento { get; set; }
+        string NumeroTelefono { get; set; }
+    }
+}
